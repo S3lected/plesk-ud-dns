198ba166e5e582b8d0c95c216e8ffc2e68209ebc
S3lected <kiefer.matthias@gmail.com>
2019-12-09 14:18:58 +0100

    Update post-install.php

--- plib/scripts/post-install.php
+++ plib/scripts/post-install.php
@@ -1,8 +1,8 @@
 <?php
 pm_Loader::registerAutoload();
-pm_Context::init('buddyns');
+pm_Context::init('uddomainreselling');
 try {
-    $script = PRODUCT_ROOT . '/bin/extension --exec ' . pm_Context::getModuleId() . ' buddyns.php';
+    $script = PRODUCT_ROOT . '/bin/extension --exec ' . pm_Context::getModuleId() . ' uddns.php';
     $result = pm_ApiCli::call('server_dns', array('--enable-custom-backend', $script));
 } catch (pm_Exception $e) {
     echo $e->getMessage() . "\n";
